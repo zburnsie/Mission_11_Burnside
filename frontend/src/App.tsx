85e4b6996ae61f0231f0ea242f44b1ad4fdef31e
zburnsie <zburnsie@byu.edu>
2025-03-29 15:41:58 -0600

    Second commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,60 @@
 import './App.css';
-import BookList from './BookList';
-import CategoryFilter from './CategoryFilter';
-import WelcomeBand from './WelcomeBand';
+import BookList from './components/BookList';
+import CategoryFilter from './components/CategoryFilter';
+import WelcomeBand from './components/WelcomeBand';
 import { useState } from 'react';
+import { CartProvider } from './context/CartContext';
+import CartSummary from './components/CartSummary';
+import CartPage from './pages/CartPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Footer from './components/Footer';
+import BackToTopButton from './components/BackToTopButton';
 
 function App() {
   const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
 
   return (
     <>
-      <div className="container mt-4">
-        <div className="row bg-primary text-white">
-          <WelcomeBand />
-        </div>
+      <Router>
+        <CartProvider>
+          <div className="container mt-4">
+            <div className="row bg-primary text-white">
+              <WelcomeBand />
+            </div>
 
-        <div className="row">
-          <div className="col-md-3">
-            <CategoryFilter
-              selectedCategories={selectedCategories}
-              setSelectedCategories={setSelectedCategories}
-            />
-          </div>
+            <div className="row">
+              <div className="col-12 text-end">
+                <CartSummary />
+              </div>
+            </div>
 
-          <div className="col-md-9">
-            <BookList selectedCategories={selectedCategories}
-            />
+            <Routes>
+              {' '}
+              {/* Define routes here */}
+              <Route
+                path="/"
+                element={
+                  <div className="row">
+                    <div className="col-md-3">
+                      <CategoryFilter
+                        selectedCategories={selectedCategories}
+                        setSelectedCategories={setSelectedCategories}
+                      />
+                    </div>
+
+                    <div className="col-md-9">
+                      <BookList selectedCategories={selectedCategories} />
+                    </div>
+                  </div>
+                }
+              />
+              <Route path="/cart" element={<CartPage />} />
+            </Routes>
           </div>
-        </div>
-      </div>
+        </CartProvider>
+      </Router>
+      <Footer/>
+      <BackToTopButton/>
     </>
   );
 }
